f64228e7b670c781b33da6205263fcbde2ef5698
Ripal Patel <ripal212007@gmail.com>
2018-10-20 16:07:01 -0500

    added some styles and fonts

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -3,13 +3,13 @@ const locationsBuilder = (arrayOfLocations)=>{
     let domString = '';
     arrayOfLocations.forEach((location) => {   
         domString += `<div id="loc" class="card loc p-2 m-3" style="width: 18rem;">
-                        <img class="card-img-top" src="${location.locationImage}" alt="Card image cap">
+                        <img class="card-img-top" src="${location.locationImage}" alt="Card image cap" width="250px" height="250px">
                         <div class="card-body card-content">
-                            <h5 class="card-title">${location.name}</h5>
-                            <p class="card-text">Address: ${location.locationAddress}</p>
+                            <h5 class="card-title"><strong>${location.name}</strong></h5>
+                            <p class="card-text"><strong>Address:</strong> ${location.locationAddress}</p>
                         </div>
                         <div class="card-body shoot-time">
-                            <h6>Shoot Time: ${location.shootTime}</h6>
+                            <h6><strong>Shoot Time:</strong> ${location.shootTime}</h6>
                         </div>
                     </div>`;
                 });
--- javascripts/components/movieComponent.js
+++ javascripts/components/movieComponent.js
@@ -2,12 +2,12 @@
 const movieBuilder = (arrayOfMovie)=>{
     let domString = '';
     arrayOfMovie.forEach((movie) => {   
-        domString += `<div class="card bg-light mb-3" style="max-width: 18rem;">
-                        <div class="card-header">${movie.name}</div>
+        domString += `<div class="card bg-light mb-3 w-75">
+                        <div class="card-header text-center movie-heading">${movie.name}</div>
                         <div class="card-body">
-                            <p class="card-text">${movie.description}</p>
-                            <h5 class="card-title">${movie.genre}</h5>
-                            <h6 class="card-title">${movie.releaseDate}</h6>
+                            <p class="card-text"><strong>Summary:</strong> ${movie.description}</p>
+                            <h6 class="card-title"><strong>Genre:</strong> ${movie.genre}</h6>
+                            <h6 class="card-title"><strong>Release Date:</strong> ${movie.releaseDate}</h6>
                         </div>
                     </div>`;
                 });
