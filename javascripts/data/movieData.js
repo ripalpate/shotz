4f59605fcbe13a8ea99b43554577836fad05ef0c
Ripal Patel <ripal212007@gmail.com>
2018-10-19 21:34:55 -0500

    added search bar

--- javascripts/data/movieData.js
+++ javascripts/data/movieData.js
@@ -1,7 +1,6 @@
 import {movieBuilder} from "../components/movieComponent.js";
 $.get('../db/movie.json')
     .done((data)=>{
-        console.log(data);
         movieBuilder(data.movie);
     })
     .fail((error)=>{
